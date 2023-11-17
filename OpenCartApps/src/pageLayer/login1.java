e735918d4d175994e5e88b92aa3a9d007fadb40d
shubhamma123 <shubhamumahajan24@gmail.com>
2023-11-17 22:39:51 +0530

    login feature

---
+++ OpenCartApps/src/pageLayer/login1.java
@@ -0,0 +1,14 @@
+package pageLayer;
+
+public class login1 {
+ public void login1() {
+	 
+	 System.out.println("hello");
+	 
+	 System.out.println("kee");
+	 
+ }
+ public void login2() {
+	 System.out.println("page layer");
+ }
+}
