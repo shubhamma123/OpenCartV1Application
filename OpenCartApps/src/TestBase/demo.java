10db519a5efa6b96968e1b771457a98e211e1251
shubhamma123 <shubhamumahajan24@gmail.com>
2023-11-17 22:10:47 +0530

    Created project Stucture

---
+++ OpenCartApps/src/TestBase/demo.java
@@ -0,0 +1,13 @@
+package TestBase;
+
+public class demo {
+ 
+	public void setup() {
+		
+		System.out.println("hello2");
+		System.out.println("hello 1");
+		System.out.println("hello v2");
+		System.out.println("add dependicies");
+		
+	}
+}
